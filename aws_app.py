2dd729dcb64b4f46153c62b4b5cd8315206fad36
Nikita Tihhomirov <dageneral@gmail.com>
2020-04-18 15:37:43 +0300

    Commited - edited some code in the file

--- aws_app.py
+++ aws_app.py
@@ -1,3 +1,15 @@
-print('hello world')
+def vat_calculation(price):
+    
+    try: 
+        price = float(price)
+        return print(price * 0.18)
+    except(ValueError, TypeError): 
+        return print ("Please check the value or the data type")
 
+if __name__ == '__main__': 
+    vat_calculation('some number') 
+
+
+
+    
         
